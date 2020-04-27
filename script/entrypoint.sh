287208e97bea8397bbb8863a864f4f19b3f7ac39
S Vineet Krishna <vineet.krishna@mindtickle.com>
2020-04-27 19:25:40 +0530

    cleanup of entrypoint

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,13 +2,5 @@
 set -ex
 
 # Render a AWS credentials with user and role for `awsudo`.
-DEFAULT_REGION=ap-southeast-1
-REGION=${REGION:-${DEFAULT_REGION}}
-
-DEFAULT_PROFILE=default
-PROFILE=${PROFILE:-${DEFAULT_PROFILE}}
-
-export KUBECONFIG=/tmp/.kube/config
-
 aws eks update-kubeconfig --name ${CLUSTER:?}
 rotate-eks-asg ${AUTOSCALING_GROUPS:?}
