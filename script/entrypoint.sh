263e02c8099c2ac2d0924e3fd2e8814eece8ae04
Yash Mehrotra <yashmehrotra95@gmail.com>
2020-05-05 18:57:42 +0530

    Fix for multi-account

--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -2,5 +2,9 @@
 set -ex
 
 # Render a AWS credentials with user and role for `awsudo`.
-aws eks update-kubeconfig --name ${CLUSTER:?}
+aws eks update-kubeconfig --name ${CLUSTER:?} --profile ${AWS_PROFILE:?}
+
+if [[ "$AWS_PROFILE" == "dev" ]]; then
+    sed -i 's/value: dev/value: default/g' ~/.kube/config
+fi
 rotate-eks-asg ${AUTOSCALING_GROUPS:?}
